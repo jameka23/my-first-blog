7dce06d28588be138ed2096450b419ce8350672b
Jameka Echols <jameka.echols@gmail.com>
2018-04-07 20:15:19 -0500

    broke site. fixing bugs

--- blog/urls.py
+++ blog/urls.py
@@ -1,23 +1,19 @@
 from django.conf.urls import include, url
-
-#from django.contrib.auth import views
-#from django.contrib.auth import logout
-
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    #url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
     #url(r'^accounts/login/$', views.login, name='login'),
     #url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'^$', views.post_list, name='post_list'),
-    #url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
-    #url(r'^accounts/login/$', views.login, name='login')
 
 
 ]

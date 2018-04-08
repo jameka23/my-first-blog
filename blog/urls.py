009ca4d33fab807eb76930477b06ded696c6d709
Jameka Echols <jameka.echols@gmail.com>
2018-04-07 20:23:14 -0500

    fixed

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,7 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
-    #url(r'^accounts/login/$', views.login, name='login'),
-    #url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),

30e682fa9f95a7b5491861c2a4a7ebe40740922c
Jameka Echols <jameka.echols@gmail.com>
2018-04-07 19:43:17 -0500

    security

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
-#from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -22,7 +22,8 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
-# New post@login_required
+# New post
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -37,7 +38,8 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
-# Edit post@login_required
+# Edit post
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -54,20 +56,23 @@ def post_edit(request, pk):
 
 
 
-# Draft a post@login_required
+# Draft a post
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
-# Publish posts@login_required
+# Publish posts
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
 
 
-# Remove posts@login_required
+# Remove posts
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
